0627493d5021b8105eddf31a81556a9ee10ac1e2
inau <inau@itu.dk>
2013-10-14 14:18:09 +0200

    sds

--- sqrt.c
+++ sqrt.c
@@ -34,76 +34,75 @@ int main(int argc, char *argv[]) {
     }
     int MAXPROCESSES = atoi(argv[2]);
     int MAXCALCS = atoi(argv[1]);
-    
+
     sum = 0;
 
     //initializing list for pids
     pthread_t tids[MAXPROCESSES];
-    
+
     /* Generate the workload structs for each processor*/
-    Workload *wlds[MAXPROCESSES];
-    
-    int z; Workload *w;
-    for(z = 0; z < MAXPROCESSES; z++) {
+    Workload * wlds[MAXPROCESSES];
+
+    int z;
+    Workload *w;
+    for (z = 0; z < MAXPROCESSES; z++) {
         w = (Workload*) malloc(sizeof (Workload));
         wlds[z] = w;
     }
-    
+
     /* Get default attributes */
     pthread_attr_init(&attr);
-    
-        int blocksize = (int) MAXCALCS / MAXPROCESSES;
-        printf("BlockSize per thread %d \n", blocksize);
-        int t = 0, i = 1;
-        for (i; i < MAXCALCS+1; i = i + blocksize) {
-            wlds[t]->_begin = i;
-            if(i+blocksize >= MAXCALCS) wlds[t]->_end = MAXCALCS+1;
+
+    int blocksize = (int) MAXCALCS / MAXPROCESSES;
+    printf("BlockSize per thread %d \n", blocksize);
+    int t = 0, i = 1;
+
+    for (i; i < MAXCALCS + 1; i = i + blocksize) {
+        wlds[t]->_begin = i;
+        if (i + blocksize >= MAXCALCS) wlds[t]->_end = MAXCALCS + 1;
+        else {
             wlds[t]->_end = i + blocksize;
-            
-            /* Create the thread */
-            pthread_create(&tid, &attr, runner, wlds[t]);
-            tids[t++] = tid;
-                        printf("T %d \t ", t);
         }
-        printf("\nOld tid\t%d\nNew tid\t%d \n", (int) tids[0], (int) tids[1]);
-        
-        //Joining all the tids
-        for(z = 0; z < MAXPROCESSES; z++) {
-            /* Wait for the given threads to exit */
-            pthread_join(tids[z], NULL);
-        }
-        printf("joins done \n");
 
-        /* Summing all the partial results*/
-        for(z = 0; z < MAXPROCESSES; z++) {
-            sum += wlds[z]->_result;
-        }
-    
-       free(w);
-        
-        printf("sum = %f\n", sum);
+        /* Create the thread */
+        pthread_create(&tid, &attr, runner, wlds[t]);
+        tids[t++] = tid;
+        printf("T %d Id %d \n", t, (int) tid);
+    }
 
-        int end = time(NULL);
-        printf("Time spent %d \n", (end - start));
+    //Joining all the tids
+    for (z = 0; z < MAXPROCESSES; z++) {
+        /* Wait for the given threads to exit */
+        pthread_join(tids[z], NULL);
+    }
+    printf("joins done \n");
+
+    /* Summing all the partial results*/
+    for (z = 0; z < MAXPROCESSES; z++) {
+        sum += wlds[z]->_result;
+    }
+
+    free(w);
+
+    printf("sum = %f\n", sum);
+
+    int end = time(NULL);
+    printf("Time spent %d \n", (end - start));
 }
 
-    /* The thread will begin control in this function */
-    void *runner(void *param) {
-        printf("Runner [ ");
-        
-        int i = ((Workload*)param)->_begin;
-        printf("Start: %d\t", i);
-        
-        int upper = ((Workload*)param)->_end;
-        printf("| End: %d\t", upper);
-        
-        ((Workload*)param)->_result = 0;
+/* The thread will begin control in this function */
+void *runner(void *param) {
+    int i = ((Workload*) param)->_begin;
 
-        for (i; i < upper; i++)
-            ((Workload*)param)->_result += sqrt(i);
-        
-        printf("| Result: %f ] \n", ((Workload*)param)->_result);
-        
-        pthread_exit(0);
-    }
-    
+    int upper = ((Workload*) param)->_end;
+
+    ((Workload*) param)->_result = 0;
+
+    for (i; i < upper; i++)
+        ((Workload*) param)->_result += sqrt(i);
+
+    printf("RUNNER [ Start: %d || End: %d || Result: %f ] \n", ((Workload*) param)->_begin, upper, ((Workload*) param)->_result);
+
+    pthread_exit(0);
+}
+
